e25fdfed8ec7bd4b9bed675c54c926628b4ea016
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2021-07-09 00:35:26 +0600

    complete task 3

---
+++ Lab - 03/1811821042/task2.asm
@@ -0,0 +1,50 @@
+.MODEL SMALL
+.STACK 100H
+.DATA
+    STRING DB "world$"
+    CONVERT DB 20H
+    
+.CODE
+    TRANSFER: 
+        MOV DX, @DATA
+        MOV DS, DX
+       
+    FOR_W:   
+        MOV AL, STRING[0] 
+        SUB AL, CONVERT 
+        MOV AH, 2H
+        MOV DL, AL
+        INT 21H 
+        
+    FOR_O:
+        MOV CL, STRING[1] 
+        SUB CL, CONVERT 
+        MOV AH, 2H
+        MOV DL, CL
+        INT 21H
+        
+    FOR_R:
+        MOV BL, STRING[2]
+        SUB BL, CONVERT
+        MOV AH, 2H
+        MOV DL, BL
+        INT 21H
+        
+    FOR_L:
+        MOV BH, STRING[3]
+        SUB BH, CONVERT
+        MOV AH, 2H
+        MOV DL, BH
+        INT 21H 
+        
+    FOR_D:
+        MOV CH, STRING[4] 
+        SUB CH, CONVERT
+        MOV AH, 2H
+        MOV DL, CH
+        INT 21H
+    
+    EXIT:
+        MOV AH, 4CH
+        INT 21H
+    
---
+++ Lab - 03/1811821042/task4.asm
@@ -0,0 +1,41 @@
+.MODEL SMALL
+.STACK 100H
+.DATA
+    VAR1 DB ?
+    VAR2 DW ?
+
+.CODE
+    DATA_TRANSFER:
+        MOV AX, @DATA
+        MOV DS, AX
+        
+     INPUT1:
+        MOV AH, 1H
+        INT 21H
+        MOV BH, AL
+
+    INPUT2:
+        MOV AH, 1H
+        INT 21H
+        MOV BL, AL 
+        
+    ADDITION:
+        SUB BH, BL  ;BH = BH - BL
+        ADD BH, 30H ;ASCII TO DECIMAL
+
+    NEWLINE:
+        MOV AH, 2H
+        MOV DL, 0AH
+        INT 21H
+        MOV AH, 2H
+        MOV DL, 0DH
+        INT 21H 
+
+    RESULT:
+        MOV AH, 2H
+        MOV DL, BH
+        INT 21H 
+        
+    EXIT:
+        MOV AH, 4CH
+        INT 21H
