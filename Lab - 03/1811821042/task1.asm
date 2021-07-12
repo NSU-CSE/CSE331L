0e9650dd8d178970b59cb9d3106c0a30e90a7c2e
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2021-07-12 21:12:53 +0600

    Update lab 3 task_1.asm

--- Lab - 03/1811821042/task1.asm
+++ Lab - 03/1811821042/task1.asm
@@ -1,54 +1,109 @@
 .MODEL SMALL
 .STACK 100H
 .DATA
-    ARRAY1 DB 1, 3, 5, 7, 9
-    ARRAY2 DB 5 DUP(?) 
-    CONVERT DB 30H
+    ARRAY1 DB 5 DUP(?)
+    ARRAY2 DB 5 DUP(?)  
+    STRING1 DB "ENTER ARRAY 1: $" 
+    STRING2 DB 0AH, 0DH, "ARRAY 2: $"
     
 .CODE 
     TRANSFER:
         MOV DX, @DATA
-        MOV DS, DX 
+        MOV DS, DX
         
-    FIRST_INDEX:
-        MOV AL, ARRAY1[0] 
-        ADD AL, CONVERT
-        MOV ARRAY2[0], AL 
-        MOV AH, 2H
-        MOV DL, ARRAY2[0]
-        INT 21H 
-        
-    SECOND_INDEX:
-        MOV AH, ARRAY1[1] 
-        ADD AH, CONVERT
-        MOV ARRAY2[1], AH 
-        MOV AH, 2H
-        MOV DL, ARRAY2[1]
-        INT 21H 
-        
-    THIRD_INDEX:
-        MOV BL, ARRAY1[2]
-        ADD BL, CONVERT
-        MOV ARRAY2[2], BL
-        MOV AH, 2H
-        MOV DL, ARRAY2[2]
+        LEA DX, STRING1
+        MOV AH, 9H
         INT 21H
         
-    FOURTH_INDEX:
-        MOV BH, ARRAY1[3]
-        ADD BH, CONVERT
-        MOV ARRAY2[3], BH
-        MOV AH, 2H
-        MOV DL, ARRAY2[3]
-        INT 21H 
+        MOV SI, 0 
         
-    FIFTH_INDEX:
-        MOV CL, ARRAY1[4] 
-        ADD CL, CONVERT
-        MOV ARRAY2[4], CL
+    INPUT1:
+        MOV AH, 1H 
+        INT 21H 
+        MOV ARRAY1[SI], AL 
+        MOV BL, ARRAY1[SI]
+        MOV ARRAY2[SI], BL 
+         
+        
+    INPUT2:
+        INC SI
+        MOV AH, 1H 
+        INT 21H 
+        MOV ARRAY1[SI], AL 
+        MOV BH, ARRAY1[SI]
+        MOV ARRAY2[SI], BH 
+         
+        
+    INPUT3:
+        INC SI
+        MOV AH, 1H 
+        INT 21H 
+        MOV ARRAY1[SI], AL 
+        MOV CL, ARRAY1[SI]
+        MOV ARRAY2[SI], CL
+        
+        
+    INPUT4:
+        INC SI
+        MOV AH, 1H 
+        INT 21H 
+        MOV ARRAY1[SI], AL 
+        MOV CH, ARRAY1[SI]
+        MOV ARRAY2[SI], CH
+         
+        
+    INPUT5:
+        INC SI
+        MOV AH, 1H 
+        INT 21H 
+        MOV ARRAY1[SI], AL 
+        MOV DH, ARRAY1[SI]
+        MOV ARRAY2[SI], DH 
+        
+    NEWLINE:
         MOV AH, 2H
-        MOV DL, ARRAY2[4]
+        MOV DL, 0AH
         INT 21H
+        MOV AH, 2H
+        MOV DL, 0DH
+        INT 21H
+        
+        
+        LEA DX, STRING2
+        MOV AH, 9H
+        INT 21H
+        
+        MOV DI, 0 
+        
+    OUTPUT1:
+        MOV AH, 2H
+        MOV DL, ARRAY2[DI]
+        INT 21H
+        
+    OUTPUT2:
+        INC DI
+        MOV AH, 2H
+        MOV DL, ARRAY2[DI]
+        INT 21H 
+         
+    OUTPUT3:
+        INC DI
+        MOV AH, 2H
+        MOV DL, ARRAY2[DI]
+        INT 21H
+         
+    OUTPUT4:
+        INC DI
+        MOV AH, 2H
+        MOV DL, ARRAY2[DI]
+        INT 21H
+         
+    OUTPUT5:
+        INC DI
+        MOV AH, 2H
+        MOV DL, ARRAY2[DI]
+        INT 21H
+        
     
     EXIT:
         MOV AH, 4CH
